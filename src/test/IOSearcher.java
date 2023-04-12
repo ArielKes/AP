bead97be5995f28231b33821fa9e7e880e047908
Tomer651 <tomer651@gamil.com>
2023-04-12 16:14:01 +0300

    fixed the source code according to assignment application

--- src/test/IOSearcher.java
+++ src/test/IOSearcher.java
@@ -1,7 +1,19 @@
 package test;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+
+interface FileSearcher {
+	public boolean search(String word, String...fileNames);
+	public void stop();
+
+}
 
 public class IOSearcher implements FileSearcher{
 
@@ -29,4 +41,50 @@ public class IOSearcher implements FileSearcher{
 	public void stop() {
 		stopMe=true;
 	}
+}
+
+class ParIOSearcher implements FileSearcher{
+
+	ExecutorService es;
+
+	public ParIOSearcher() {
+		es= Executors.newCachedThreadPool();
+	}
+
+	public boolean search(String word, String...fileNames){
+		ArrayList<IOSearcher> searchers=new ArrayList<>();
+		ArrayList<Future<Boolean>> fs=new ArrayList<>();
+
+		for(String fn : fileNames) {
+			IOSearcher s = new IOSearcher();
+			searchers.add(s);
+			fs.add(es.submit(()->{
+				boolean found = s.search(word, fn);
+				if(found) {
+					searchers.forEach(srch->srch.stop());
+				}
+				return found;
+			}));
+		}
+
+		boolean found = false;
+		for(Future<Boolean> f : fs) {
+			try {
+				found|=f.get();
+			} catch (InterruptedException | ExecutionException e) {}
+		}
+
+		return found;
+	}
+
+	public void stop() {
+		es.shutdownNow();
+	}
+
+	@Override
+	public void finalize() {
+		es.shutdown();
+	}
+
+
 }
