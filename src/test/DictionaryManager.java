64c9a1cdd26a86e3d960ec9b5026bfd414d843d5
Tomer651 <tomer651@gamil.com>
2023-04-12 16:52:54 +0300

    merged 2 almost identical function into 1

--- src/test/DictionaryManager.java
+++ src/test/DictionaryManager.java
@@ -22,7 +22,7 @@ public class DictionaryManager {
         }
     }
 
-    public boolean query(String...args) {
+    private boolean dictionaryHandler(char command, String...args) {
         //get the question from the args
         String question = args[args.length - 1];
 
@@ -31,30 +31,23 @@ public class DictionaryManager {
             //Check if books is in library
             if(!library.containsKey(args[i]))
                 library.put(args[i],new Dictionary(args[i]));
+
             //go over all the dictionaries in the library and
             //if the word is in one of them return true
-            if(library.get(args[i]).query(question))
+
+            //if command is C
+            if(command =='C' && library.get(args[i]).challenge(question))
+                return true;
+            //if command is 'Q'
+            if(command =='Q' && library.get(args[i]).query(question))
                 return true;
         }
         return false;
     }
 
-    public boolean challenge(String...args) {
-        //get the question from the args
-        String question = args[args.length - 1];
+    public boolean query(String...args) { return dictionaryHandler('Q', args); }
 
-        //check all fo the args (books names), if we do not have this book name in the library, make one
-        for (int i = 0;i < args.length - 1;i++){
-            //Check if books is in library
-            if(!library.containsKey(args[i]))
-                library.put(args[i],new Dictionary(args[i]));
-            //go over all the dictionaries in the library and
-            //if the word is in one of them return true
-            if(library.get(args[i]).challenge(question))
-                return true;
-        }
-        return false;
-    }
+    public boolean challenge(String...args) { return dictionaryHandler('C', args); }
 
     public int getSize() { return library.size(); }
 }
