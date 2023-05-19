1ce3c2e5cb01b2d8c62671510e1bae64a2f33a73
Tomer651 <tomer651@gamil.com>
2023-05-19 18:04:17 +0300

    added view to the project

--- src/view/HelloController.java
+++ src/view/HelloController.java
@@ -1,14 +1,124 @@
 package view;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.ResourceBundle;
 
-public class HelloController {
+public class HelloController implements Initializable {
     @FXML
-    private Label welcomeText;
+    private Button A;
+    @FXML
+    private Button B;
+    @FXML
+    private Button C;
+    @FXML
+    private Button D;
+    @FXML
+    private Button E;
+    @FXML
+    private Button F;
+    @FXML
+    private Button G;
+    @FXML
+    private Button H;
+    @FXML
+    private Button I;
+    @FXML
+    private Button J;
+    @FXML
+    private Button K;
+    @FXML
+    private Button L;
+    @FXML
+    private Button M;
+    @FXML
+    private Button N;
+    @FXML
+    private Button O;
+    @FXML
+    private Button P;
+    @FXML
+    private Button Q;
+    @FXML
+    private Button R;
+    @FXML
+    private Button S;
+    @FXML
+    private Button T;
+    @FXML
+    private Button U;
+    @FXML
+    private Button V;
+    @FXML
+    private Button W;
+    @FXML
+    private Button X;
+    @FXML
+    private Button Y;
+    @FXML
+    private Button Z;
+    private ArrayList<Button> buttons;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    BoardDisplayer boardDisplayer = new BoardDisplayer();
+
+    int[][] board={
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,0,1,1,1,0,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,0,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,0,1,1,1,1,0,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,0,1,1,1,1,1,1,0,1,1},
+            {1,1,1,1,1,1,1,1,1,1,1,1,0,1,1},
+            {1,1,1,1,1,1,1,1,1,1,1,1,0,1,1},
+            {1,1,1,1,1,0,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1,0,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,0,1,0,0,0,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+    };
+    private boolean choseLatter;
+    private String latter;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        buttons = new ArrayList<>(Arrays.asList(A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z));
+
+        buttons.forEach(button ->{
+            setupButton(button);
+            button.setFocusTraversable(false);
+        });
+        choseLatter = false;
+        latter = new String();
+        boardDisplayer.setBoardData(board);
+    }
+
+    private void setupButton(Button button) {
+        button.setOnMouseClicked(mouseEvent -> {
+            resetButtonsView(button);
+            setPlayerChoice(button);
+            button.setDisable(true);
+        });
+    }
+
+    public void resetButtonsView(Button button){
+        for(Button b:buttons){
+            b.setDisable(false);
+        }
+    }
+    public void setPlayerChoice(Button button){
+        latter = button.getText();
+        if (latter.length() > 1)
+            latter = null;
+        choseLatter = true;
     }
 }
