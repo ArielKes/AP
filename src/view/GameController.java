16808f6660c9807d785df9005e00f08b8a305ad0
arielkeslassy <ariel.keslassy@weizmann.ac.il>
2023-05-26 16:34:41 +0300

    added support in glowing rectangles and scores displays

--- src/view/GameController.java
+++ src/view/GameController.java
@@ -6,11 +6,17 @@ import javafx.fxml.Initializable;
 import java.net.URL;
 import java.util.*;
 
+import javafx.scene.control.Label;
+import javafx.util.Duration;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import view_model.ViewModel;
 
@@ -69,17 +75,21 @@ public class GameController extends BaseController implements Observer,Initializ
     private Button Z;
     @FXML
     private Button delete;
+    @FXML
+    Button testButton;
+    @FXML
+    Label score;
     private ArrayList<Button> buttons;
 
     @FXML
     private GridPane gridPane;
 
-    private int gridSize = 15;
+    private final int gridSize = 15;
 
     private boolean letterChosen = false;
     private Text letter;
-
     ViewModel vm;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         buttons = new ArrayList<>(Arrays.asList(A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,delete));
@@ -88,6 +98,7 @@ public class GameController extends BaseController implements Observer,Initializ
             setupButton(button);
             button.setFocusTraversable(false);
         });
+        testButton.setOnMouseClicked(this::test);
         //initialize gridPane
         gridPane.setGridLinesVisible(true);
         for (int row = 0; row < 15; row++) {
@@ -100,6 +111,15 @@ public class GameController extends BaseController implements Observer,Initializ
                 gridPane.add(pane, col, row);
             }
         }
+    }
+
+    private void test(Event event) {
+        wordVerifiedDisplay(true, 0, 0, 0, 4);
+        Map<String, Integer> map = new HashMap<>();
+        map.put("A", 1);
+        map.put("B", 3);
+        map.put("C", 3);
+        setScores(map);
     }
 
     private void setupButton(Button button) {
@@ -124,22 +144,77 @@ public class GameController extends BaseController implements Observer,Initializ
         }
     }
 
-    public void resetButtonsView(Button button){
-        for(Button b:buttons){
-            b.setDisable(false);
-        }
-    }
-    public void setPlayerChoice(Button button){
+    private void setPlayerChoice(Button button){
         // get the letter from the clicked button
         letter = new Text(button.getText());
         letterChosen = true;
     }
 
-    public void changeColor(int row, int col, Color color){
+    private void changeColor(int row, int col, Color color){
         StackPane pane = (StackPane) gridPane.getChildren().get((row * gridSize) + col + 1);
         Rectangle r = (Rectangle) pane.getChildren().get(0);
         r.setFill(color);
 
     }
+
+    private void glowRectangle(int row, int col, Color color){
+        /*
+        make the rectanle glow in a given color and change it back to the original color
+         */
+        StackPane pane = (StackPane) gridPane.getChildren().get((row * gridSize) + col + 1);
+        Rectangle r = (Rectangle) pane.getChildren().get(0);
+        Timeline timeline = new Timeline();
+        timeline.setCycleCount(2);
+        timeline.setAutoReverse(true);
+        KeyValue kv = new KeyValue(r.fillProperty(), color);
+        KeyFrame kf = new KeyFrame(Duration.millis(500), kv);
+        timeline.getKeyFrames().add(kf);
+        timeline.play();
+    }
+
+//    ************************************************** API **************************************************
+
+    public void resetButtonsView(Button button){
+        for(Button b:buttons){
+            b.setDisable(false);
+        }
+    }
+
+    public void wordVerifiedDisplay(boolean verified, int startRow, int startCol, int endRow, int endCol){
+        for (int row = startRow; row <= endRow; row++) {
+            for (int col = startCol; col <= endCol; col++) {
+                if (verified)
+                    glowRectangle(row, col, Color.GREEN);
+                else
+                    glowRectangle(row, col, Color.RED);
+            }
+        }
+    }
+
+    public void setScores(Map<String, Integer> dict){
+        // set the scores in the view
+        StringBuilder text = new StringBuilder("Scores: ");
+        for (Map.Entry<String, Integer> entry : dict.entrySet()) {
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+            text.append(key).append(": ").append(value).append(", ");
+        }
+        score.setText(text.toString());
+
+    }
+
+    public void setViewModel(ViewModel vm) {
+        this.vm = vm;
+        vm.addObserver(this);
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        if (o == vm) {
+            // update the view
+        }
+    }
+
+
 
 }
