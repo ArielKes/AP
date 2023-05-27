d2885a75ad85ba75e7b6541f027ba309ca8eab50
Tomer651 <tomer651@gamil.com>
2023-05-27 10:53:32 +0300

    added buttons for the game

--- src/view/GameController.java
+++ src/view/GameController.java
@@ -1,5 +1,6 @@
 package view;
 
+import javafx.beans.property.*;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -16,73 +17,30 @@ import view_model.ViewModel;
 
 public class GameController extends BaseController implements Observer,Initializable {
     @FXML
-    private Button A;
-    @FXML
-    private Button B;
-    @FXML
-    private Button C;
-    @FXML
-    private Button D;
-    @FXML
-    private Button E;
-    @FXML
-    private Button F;
-    @FXML
-    private Button G;
-    @FXML
-    private Button H;
-    @FXML
-    private Button I;
-    @FXML
-    private Button J;
-    @FXML
-    private Button K;
-    @FXML
-    private Button L;
-    @FXML
-    private Button M;
-    @FXML
-    private Button N;
-    @FXML
-    private Button O;
-    @FXML
-    private Button P;
-    @FXML
-    private Button Q;
-    @FXML
-    private Button R;
-    @FXML
-    private Button S;
-    @FXML
-    private Button T;
-    @FXML
-    private Button U;
-    @FXML
-    private Button V;
-    @FXML
-    private Button W;
-    @FXML
-    private Button X;
-    @FXML
-    private Button Y;
-    @FXML
-    private Button Z;
-    @FXML
-    private Button delete;
+    private Button A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,delete;
     private ArrayList<Button> buttons;
 
     @FXML
     private GridPane gridPane;
-
     private int gridSize = 15;
 
     private boolean letterChosen = false;
+    BooleanProperty ver = new SimpleBooleanProperty();
+    BooleanProperty check = new SimpleBooleanProperty();
+    IntegerProperty col = new SimpleIntegerProperty();
+    IntegerProperty row = new SimpleIntegerProperty();
+    StringProperty word = new SimpleStringProperty();
     private Text letter;
 
     ViewModel vm;
+    /*public GameController(ViewModel vm){
+        this.vm = vm;
+    }*/
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         buttons = new ArrayList<>(Arrays.asList(A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,delete));
+        resetChoice();
 
         buttons.forEach(button ->{
             setupButton(button);
@@ -116,6 +74,11 @@ public class GameController extends BaseController implements Observer,Initializ
             StackPane pane = (StackPane) event.getSource();
             Text text = (Text) pane.getChildren().get(1);
             if (text.getText().equals("") || letter.getText().equals(""))  {
+                if(word.get().equals("")){
+                    col.set((int)(pane.getLayoutX()/pane.getLayoutBounds().getWidth()));
+                    row.set((int)(pane.getLayoutY()/pane.getLayoutBounds().getHeight()+1));
+                }
+                word.set(word.get()+letter.getText());
                 text.setText(letter.getText());
 
                 // reset letter
@@ -134,6 +97,11 @@ public class GameController extends BaseController implements Observer,Initializ
         letter = new Text(button.getText());
         letterChosen = true;
     }
+    public void resetChoice(){
+        ver.set(false);
+        check.set(false);
+        word.set("");
+    }
 
     public void changeColor(int row, int col, Color color){
         StackPane pane = (StackPane) gridPane.getChildren().get((row * gridSize) + col + 1);
@@ -141,5 +109,33 @@ public class GameController extends BaseController implements Observer,Initializ
         r.setFill(color);
 
     }
+
+    @FXML
+    public void verticalButtonPushed(){
+        if(ver.get())
+            ver.set(false);
+        else
+            ver.set(true);
+        System.out.println("vertical "+vm.vertical.get());
+    }
+
+    public void checkButtonPushed(){
+        check.set(true);
+        vm.trySetWord();
+        System.out.println(vm.word.get());
+        System.out.println("col: " + vm.col.get() + "row: " + vm.row.get());
+        resetChoice();
+    }
+
+    public void setViewModel(ViewModel vm){
+        this.vm = vm;
+        vm.vertical.bind(this.ver);
+        vm.check.bind(this.check);
+        vm.word.bind(this.word);
+        vm.col.bind(this.col);
+        vm.row.bind(this.row);
+    }
+
+
 
 }
