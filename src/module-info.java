d9db24115f78631f7490464d3213ba1fdca672c2
Tomer651 <tomer651@gamil.com>
2023-07-15 21:59:48 +0300

    add dependencies with mongoDB

--- src/module-info.java
+++ src/module-info.java
@@ -2,9 +2,13 @@ module view {
     requires javafx.controls;
     requires javafx.fxml;
             
-        requires org.controlsfx.controls;
-                        requires org.kordamp.bootstrapfx.core;
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
     requires java.management;
+
+    requires org.mongodb.driver.sync.client;
+    requires org.mongodb.driver.core;
+    requires org.mongodb.bson;
 
     opens view to javafx.fxml;
     exports view;
